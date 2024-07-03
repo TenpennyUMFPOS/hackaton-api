4a12b181a5d464ccb754ca79c772a1dc2e3a02f0
TenpennyUMFPOS <med19aziz@gmail.com>
2024-07-03 16:21:12 +0100

    index changes

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const port = 7000;
+const port = process.env.PORT || 7000; // Use process.env.PORT for Vercel compatibility
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,14 +23,16 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log('DB connection error:', error));
 db.once('open', () => console.log('Connected to database'));
 
+// Import and use your routes
+const tokenRoutes = require('./routes/tokenRoutes');
+app.use('/api', tokenRoutes); // Prefix routes with /api for better organization
 
+// Catch-all route for undefined endpoints
+app.use((req, res) => {
+    res.status(404).send('Sorry, the page you are looking for does not exist.');
+});
 
-
-
-
-
-const tokenRoutes = require("./routes/tokenRoutes");
-app.use(tokenRoutes)
+// Start the server
 app.listen(port, () => {
     console.log('Server is listening at port ' + port);
 });
